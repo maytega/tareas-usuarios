9810e881cfab9331c0887721b4747e5ff9014b5b
maytega <maytega@uniovi.es>
2025-04-24 17:28:14 +0200

    tasks and users

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require("express")
+let app = express()
+
+let tasks = [{id:1, text:"hacer la compra"},
+             {id:2, text:"revisar facturas"}
+]
+
+let users = [{id:1, email:"user1", password:"user1"},
+             {id:2, email:"user2", password:"user2"}
+]
+
+app.get("/tasks", (req, res) => {
+    res.send(tasks)
+})
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+
+app.listen(8081, () => {
+    console.log("Servidor escuchando ...")
+}
+)
